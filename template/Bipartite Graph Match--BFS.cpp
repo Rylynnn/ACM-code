ac807ec1340f2173c51296889ad4343fe8c6a707
igoodvegetablea <lzy960601@outlook.com>
2016-09-07 19:41:16 +0800

    update Bipartite Graph Match------BFS&&DFS

---
+++ template/Bipartite Graph Match--BFS.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstring>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+  
+using namespace std;
+  
+const double eps=1e-10;
+const double pi=3.1415926535897932384626433832795;
+const double eln=2.718281828459045235360287471352;
+  
+#define LL long long
+#define IN freopen("in.txt", "r", stdin)
+#define OUT freopen("out.txt", "w", stdout)
+#define scan(x) scanf("%d", &x)
+#define scan2(x, y) scanf("%d%d", &x, &y)
+#define scan3(x, y, z) scanf("%d%d%d", &x, &y, &z)
+#define sqr(x) (x) * (x)
+#define pr(x) printf("Case %d: ",x)
+#define prn(x) printf("Case %d:\n",x)
+#define prr(x) printf("Case #%d: ",x)
+#define prrn(x) printf("Case #%d:\n",x)
+#define lc o << 1
+#define rc o << 1 | 1
+#define lowbit(x) (x&(-x))
+#define min(x,y) ((x)<(y)?(x):(y))
+#define max(x,y) ((x)>(y)?(x):(y))
+
+struct Edge
+{
+    int from;
+    int to;
+    int weight;
+
+    Edge(int f, int t, int w):from(f), to(t), weight(w) {}
+};
+
+vector<int> G[__maxNodes]; /* G[i] 存储顶点 i 出发的边的编号 */
+vector<Edge> edges;
+typedef vector<int>::iterator iterator_t;
+int num_nodes;
+int num_left;
+int num_right;
+int num_edges;
+
+queue<int> Q;
+int prev[__maxNodes];
+int Hungarian()
+{
+    int ans = 0;
+    memset(matching, -1, sizeof(matching));
+    memset(check, -1, sizeof(check));
+    for (int i=0; i<num_left; ++i) {
+        if (matching[i] == -1) {
+            while (!Q.empty()) Q.pop();
+            Q.push(i);
+            prev[i] = -1; // 设 i 为路径起点
+            bool flag = false; // 尚未找到增广路
+            while (!Q.empty() && !flag) {
+                int u = Q.front();
+                for (iterator_t ix = G[u].begin(); ix != G[u].end() && !flag; ++ix) {
+                    int v = edges[*ix].to;
+                    if (check[v] != i) {
+                        check[v] = i;
+                        Q.push(matching[v]);
+                        if (matching[v] >= 0) { // 此点为匹配点
+                            prev[matching[v]] = u;
+                        } else { // 找到未匹配点，交替路变为增广路
+                            flag = true;
+                            int d=u, e=v;
+                            while (d != -1) {
+                                int t = matching[d];
+                                matching[d] = e;
+                                matching[e] = d;
+                                d = prev[d];
+                                e = t;
+                            }
+                        }
+                    }
+                }
+                Q.pop();
+            }
+            if (matching[i] != -1) ++ans;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+	
+    return 0;
+}
+
